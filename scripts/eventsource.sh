144ae1a807d834396c9f8344125276772372d7ce
yaroslav-codefresh <yaroslav@codefresh.io>
2021-08-20 14:36:03 +0300

    update

--- scripts/eventsource.sh
+++ scripts/eventsource.sh
@@ -1,3 +1,3 @@
 source ./namespace.sh
 
-kubectl -n $NAMESPACE port-forward deployment/github-eventsource 120000:12000
+kubectl -n $NAMESPACE port-forward "$(kubectl -n $NAMESPACE get po | grep github-eventsource | awk '{print $1}')" 12000:12000
